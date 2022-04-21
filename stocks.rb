e08c0c83b3dce11f9e417fbf7ccb43042768e4d5
Gregory Cohen <gregorycohen2@gmail.com>
2022-04-21 19:57:48 -0400

    actualdemocracy.net

--- stocks.rb
+++ stocks.rb
@@ -1,5 +1,15 @@
 #!/usr/bin/crystal
 #class Aray
+class Object
+def +(obj : String)
+	to_s + obj
+end
+end
+
+
+class BadStock < Exception
+end
+
 
 class Array
 	def diff
@@ -8,40 +18,131 @@ class Array
 	end
 end
 
-def procedss(n)
+def check_if_low(i)
+i.split[1..-3].each do |i|
+		if i.to_f <= 2.0
+			raise BadStock.new
+		end
+	end
+end
+
+
+#	end; end
+
+def get_diff(i)
+i.split[3..4].reverse.diff
+end
+
+
+def process(n : String)
+#def procedss(n : Strin)
 	a =n.strip.split("\n").reverse
 #	puts a
 #	a = a
 count=0
+percent_sum = percent_sum2 = percent_sum3 = percent_sum4 = percent_sum5 = 0.0
 str=""
+	if ARGV.size > 0 && ARGV[0] == "q"
+		q = true
+	end
 	a.each do |i|
-		if i[0] >= '0' && i[0] <= '9'
-		str+=(v=i.split[3..4].reverse.diff).to_s + " "
+		check_if_low(i)
+#			v=get_diff#).to_s + " "
+			v = get_diff i
+						percent_sum += v
+#		end
 		if v.negative?
 			count += 1
 		end
+#		end
+	end
+
+	last = a.last 50
+
+#	last.each do 
+
+
+	  last.each do |i|
+	  	percent_sum2 +=    get_diff i
+	  end
+
+	last = a.last 5
+
+#	last.each do 
+
+
+	  last.each do |i|
+	  	percent_sum3 +=    get_diff i
+	  end
+
+	last = a.last 10
+
+#	last.each do 
+
+
+	  last.each do |i|
+	  	percent_sum4 +=    get_diff i
+	  end
+
+	last = a.last 90
+
+#	last.each do 
+
+
+	  last.each do |i|
+	  	percent_sum5 +=    get_diff i
+	  end
+
+	  
+	c = :cyan
+	l = [percent_sum, percent_sum2, percent_sum3, percent_sum4, percent_sum5
+	]
+
+	l.each do |i|
+		if i.negative?
+raise BadStock.new #			raise BadStock
 		end
 	end
+	str += "overall 50 50 10 5 90"
+str += "%"
+	str += " percent sum"
 	str += [count, "had lower market close than open", count,"of",a.size].join " "
+#	enbd
+#	end
 	str
 end
 
-class Object
+class Array
 def prices#args=0
-args = self
-#puts 5
-a = "https://query1.finance.yahoo.com/v7/finance/quote?lang=en-CA&region=CA&corsDomain=ca.finance.yahoo.com&fields=symbol,marketState,regularMarketPrice,regularMarketChange,regularMarketChangePercent,preMarketPrice,preMarketChange,preMarketChangePercent,postMarketPrice,postMarketChange,postMarketChangePercent&symbols=" + args.join(",")
+
 #puts a
-#puts a
-#a =  Process.popen("curl -s '#{a}'").read
-#Process.run
+args = [] of typeof("")
+
+
+
+each do |i|
+begin
+if(!i.nil?)
+[i].old_prices
+
+args.push i# rescue : String  next
+end
+rescue e : BadStock
+#	puts "Not good stock"
+	next
+end
+
+end
+
+
+while true
+a = "https://query1.finance.yahoo.com/v7/finance/quote?lang=en-CA&region=CA&corsDomain=ca.finance.yahoo.com&fields=symbol,marketState,regularMarketPrice,regularMarketChange,regularMarketChangePercent,preMarketPrice,preMarketChange,preMarketChangePercent,postMarketPrice,postMarketChange,postMarketChangePercent&symbols=" +               args.join(',')
+puts args.size + "stocks being got"
 Process.run "curl", args: {"-s", a} do |p|
 	a = p.output.gets_to_end
 #end
 
 end
-
-#puts a
 
 
 a = JSON.parse(a)
@@ -67,35 +168,38 @@ a.each do |i|
 
 rank += 1;	
 
-
 if (!i.as_h.has_key? "regularMarketPrice"); next; end
-
+s=i["symbol"]
 
 list.push  ([
-		[rank,".",s=i["symbol"],  (i["regularMarketPrice"].as_f.to_s || (0.0/0).to_s)].join(""),              	"failing" ,       
+
+		[rank,".",s,  (i["regularMarketPrice"].as_f.to_s || (0.0/0).to_s)].join(""),              	"failing" ,       
 	].join.colorize(:green).bold.to_s + #" failing " + \
-	i["regularMarketChangePercent"].to_s.colorize(:red).to_s + [s].old_prices(20).join(" "))
+	i["regularMarketChangePercent"].to_s.colorize(:red).to_s)
 #	p = i["regularMarketPrice"]
-#	end
-end
-
-list
-#puts list
-
-#end
-#end
-#end
-#end
 end
 
 
-def old_prices(n=5)
+
+system "tput reset"
+puts list.join("\n")
+end
+
+
+end
+
+
+
+
+
+
+def old_prices()
 #each do
 lines=[] of typeof("")
 
 to_a.each do |i|
 
-            lines.push       procedss(`tail /home/a/s/#{i} -n #{n}`)
+            lines.push       process(`tail /home/a/s/#{i} -n +3`)
 end
 
 return lines
@@ -104,23 +208,34 @@ return lines
 end
 
 end
-
+require "file_utils"
+#include FileUtils      
 
 def main
-while (a = STDIN.read_line.split) != nil
-	a.prices.each do |k|
-	puts k;end
-#		puts(k, v*1.05); end
+	FileUtils.cd "/home/a/s/"
+glob = Dir.glob("*").map do |i|
+	if File.read(i).size < 29
+		nil
+	else
+		i
+	end
 end
-puts `grep -Fv Fewest $HOME/D | grep -F '1, 1, 1' | t i.split[0]`.split.prices
-puts `grep -Fv Fewest $HOME/D | grep -F '1, 1, 1' | t i.split[0]`.split.old_prices
+glob.compact!
+
+puts "Read globs"
+
+#if glob.nil?
+#	return
+#end
+glob.prices
+#		puts(k, v*1.05); end
+#end
+#exit
+#puts `grep -Fv Fewest $HOME/D | grep -F '1, 1, 1' | t i.split[0]`.split.prices
+#puts `grep -Fv Fewest $HOME/D | grep -F '1, 1, 1' | t i.split[0]`.split.old_prices
 end
 
 require "json"
 require "colorize"
 main
-
-
-
-
 
